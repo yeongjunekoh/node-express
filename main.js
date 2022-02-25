9280a92c7e6e06c0409585277b098c4d1858eed0
yeongjunekoh <rhdudwns0@naver.com>
2022-02-25 17:18:47 +0900

    [FIX] Delete useless code

--- main.js
+++ main.js
@@ -44,23 +44,3 @@ var app = http.createServer(function(request,response){
 app.listen(3000);
 
 // 이걸 자유자재로 다루는 것이 이 수업의 목표
-
-// var http = require('http');
-// var fs = require('fs');
-// var url = require('url');
- 
-// var app = http.createServer(function(request,response){
-//     var _url = request.url;
-//     var queryData = url.parse(_url, true).query;
-//     console.log(queryData.id);
-//     if(_url == '/'){
-//       _url = '/index.html';
-//     }
-//     if(_url == '/favicon.ico'){
-//       return response.writeHead(404);
-//     }
-//     response.writeHead(200);
-//     response.end(queryData.id);
- 
-// });
-// app.listen(3000);
