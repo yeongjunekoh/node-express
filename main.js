764db65c7ac21fc3c11d753da96087a0bceb4b2b
yeongjunekoh <rhdudwns0@naver.com>
2022-02-25 18:23:25 +0900

    [STUDY] According to pathname, found Error for 404(Not Found)

--- main.js
+++ main.js
@@ -6,48 +6,43 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
 
-    var template = `
-        <!doctype html>
-            <html>
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ul>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ul>
-                    <h2>${title}</h2>
-                </body>
-            </html>
-    `;
+    if (_url=='/') title = "Welcome";
 
-    if(_url == '/'){
-        title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
+    if(pathname === '/'){
+        try{
+            fs.readFile(`data/${title}`, "utf-8", function(err, description) {
+                var template = `
+                    <!doctype html>
+                        <html>
+                            <head>
+                                <title>WEB1 - ${title}</title>
+                                <meta charset="utf-8">
+                            </head>
+                            <body>
+                                <h1><a href="/">WEB</a></h1>
+                                <ul>
+                                    <li><a href="/?id=HTML">HTML</a></li>
+                                    <li><a href="/?id=CSS">CSS</a></li>
+                                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                                </ul>
+                                <h2>${title}</h2>
+                                ${description}
+                            </body>
+                        </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            })
+        } catch (error) {
+            response.end("에러가 발생하였습니다.");
+        }
+    } else {
         response.writeHead(404);
-        response.end();
-        return;
+        response.end("not Found");
     }
-    
-    response.writeHead(200);
-
-    try{
-        fs.readFile(`data/${title}`, "utf-8", function(err, data) {
-            // console.log(data);
-            // var content = data;
-            response.end(template + data);
-        })
-    } catch (error) {
-        response.end(template + "에러가 발생하였습니다.");
-    }
-    
 });
 
 app.listen(3000);
