cc54458d9a732d24cbc58e74aca74f15a5829f22
yeongjunekoh <rhdudwns0@naver.com>
2022-03-02 15:23:33 +0900

    [STUDY] How to use conditional statement in server
    - same in frontend

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
 //url 모듈을 사용
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
-    var basicDescription;
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  var title = queryData.id;
+  var basicDescription;
 
-    if (_url=='/') {
-        title = "Welcome";
-        basicDescription = "Hello, this is main page";
-    };
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      title = "Welcome";
+      basicDescription = "Hello, this is main page";
+    }
 
-    if(pathname === '/'){
-        try{
-            fs.readFile(`data/${title}`, "utf-8", function(err, description) {
-                var template = `
+    try {
+      fs.readFile(`data/${title}`, "utf-8", function (err, description) {
+        var template = `
                     <!doctype html>
                         <html>
                             <head>
@@ -37,40 +37,39 @@ var app = http.createServer(function(request,response){
                             </body>
                         </html>
                 `;
-                response.writeHead(200);
-                response.end(template);
-            })
-        } catch (error) {
-            response.end("에러가 발생하였습니다.");
-        }
-    } else {
-        response.writeHead(404);
-        response.end("not Found");
+        response.writeHead(200);
+        response.end(template);
+      });
+    } catch (error) {
+      response.end("에러가 발생하였습니다.");
     }
+  } else {
+    response.writeHead(404);
+    response.end("not Found");
+  }
 });
 
 app.listen(3000);
 
 // 이걸 자유자재로 다루는 것이 이 수업의 목표
 
+// console.log(_url, queryData.id);
+/* queryString으로 값을 변경할 수 있음 */
 
-    // console.log(_url, queryData.id);
-    /* queryString으로 값을 변경할 수 있음 */
-
-    // console.log(__dirname + _url);
-    /* 
+// console.log(__dirname + _url);
+/* 
         이 코드는 console에 나오는 것이다. 사용자가 요청할 때마다 위의 요청에 따라
         우리가 읽어들이는 파일을 만들수 있게 된다.
         response.end(fs.readFileSync(__dirname + _url));
      */
 
-    // console.log(fs.readFileSync(__dirname + _url));
-    /* 
+// console.log(fs.readFileSync(__dirname + _url));
+/* 
         해당 콘솔은 파일을 읽음 -> 16진수로 표현되는데
     */
-    
-    // response.end("robert june" + _url);
-    /* 
+
+// response.end("robert june" + _url);
+/* 
         "robert june" + _url 스트링을 반환하게 된다.
         response.end() 메서드 내부에 어떤 값을 넣느냐에 따라서 사용자에게 전송하는 데이터가 바뀐다.
         node.js, phython, jango, PHP 등이 할 수 있는것
